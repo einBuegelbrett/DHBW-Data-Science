8226cd94652b9686b5513cdf676ad63c21d017d4
Sven Sendke <svensendke@gmail.com>
2024-12-26 17:07:33 +0100

    feat: add statistics

---
+++ eda/statistiken.py
@@ -0,0 +1,43 @@
+import pandas as pd
+import numpy as np
+from scipy import stats
+
+def relative_haeufigkeit(data: pd.Series) -> pd.Series:
+    """
+    Calculate the relative frequency of each unique value in a Series.
+
+    :param data: Input Series
+    :return: Series with the relative frequencies
+    """
+    return data.value_counts(normalize=True)
+
+
+def mittelwert(data: pd.Series) -> float:
+    """
+    Calculate the arithmetic mean of a Series.
+
+    :param data: Input Series
+    :return: Arithmetic mean
+    """
+    return data.mean()
+
+
+def median(data: pd.Series) -> float:
+    """
+    Calculate the median of a Series.
+
+    :param data: Input Series
+    :return: Median
+    """
+    return data.median()
+
+
+def modus(data: pd.Series) -> float:
+    """
+    Calculate the mode of a Series.
+
+    :param data: Input Series
+    :return: Mode
+    """
+    return data.mode().iloc[0]
+
