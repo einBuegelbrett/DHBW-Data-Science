966da46e8bdc26de306df15df4e738da2203be66
Sven Sendke <svensendke@gmail.com>
2024-12-28 11:03:32 +0100

    feat: add first steps for data cleaning in main

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-import datenvorverarbeitung as dv
+import datenvorverarbeitung.datenvorverarbeitung as dv
 
 if __name__ == "__main__":
-    df = dv.read_document("./datasets/Social-Media-Datensatz.csv")
-    print(df.head())
+    document = "./datasets/Social-Media-Datensatz.csv"
+    df = dv.read_document(document)
+    df = dv.replace_missing_values(df)
+    df = dv.remove_duplicates(df)
+
+
