10621dff53e3127548d98c71bdc461847532ba17
Sven Sendke <svensendke@gmail.com>
2024-12-28 14:07:37 +0100

    feat: add boxplot

--- eda/visualisierungen.py
+++ eda/visualisierungen.py
@@ -1,3 +1,14 @@
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+def boxplot(data, x, y, hue, title, x_label, y_label, save_path):
+    plt.figure(figsize=(15, 10))
+    sns.boxplot(data=data, x=x, y=y, hue=hue)
+    plt.title(title)
+    plt.xlabel(x_label)
+    plt.ylabel(y_label)
+    plt.savefig(save_path)
+    plt.show()
 
 
 def scatterplot(data, column1, column2):
