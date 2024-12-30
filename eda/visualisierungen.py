b7363ec4a6210b5bb51336176c7023cca9468ecf
Sven Sendke <svensendke@gmail.com>
2024-12-30 11:35:43 +0100

    feat: add social-media and pie_chart

--- eda/visualisierungen.py
+++ eda/visualisierungen.py
@@ -15,3 +15,19 @@ def scatterplot(data, column1, column2):
     plt.figure(figsize=(15, 10))
     data.plot.scatter(x=column1, y =column2)
     plt.show()
+
+
+def pie_chart(data, column, title):
+    """
+    Creates a pie chart based on a specific column of a DataFrame.
+
+    :param data: DataFrame containing the data.
+    :param column: The name of the column in the DataFrame to be used for the chart.
+                   The column should contain categorical data or data that can be grouped into categories.
+    :param title: The title of the pie chart, displayed at the top of the chart.
+    :return: None.
+    """
+    plt.figure(figsize=(15, 10))
+    data[column].value_counts().plot.pie(autopct='%1.1f%%')
+    plt.title(title)
+    plt.show()
--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import datenvorverarbeitung.datenvorverarbeitung as dv
 import datensaetze_funktionen.kunden.kunden as kd
+import datensaetze_funktionen.social_media.social_media as sm
 
 if __name__ == "__main__":
     document = "./datasets/Kunden-Datensatz.csv"
@@ -9,3 +10,6 @@ if __name__ == "__main__":
 
     if document == "./datasets/Kunden-Datensatz.csv":
         kd.kunden_main(df)
+
+    if document == "./datasets/Social-Media-Datensatz.csv":
+        sm.social_media_main(df)
