eee8118902bf03232f8983f297ffae2b6c9107b0
Sven Sendke <svensendke@gmail.com>
2025-01-07 21:17:33 +0100

    fix: add average to evaluate model

--- ml/evaluate_model.py
+++ ml/evaluate_model.py
@@ -1,6 +1,6 @@
 from sklearn.metrics import accuracy_score, f1_score, precision_score, recall_score, classification_report
 
-def evaluate_model(y_true, y_pred):
+def evaluate_model(y_true, y_pred, average='weighted'):
     """
     Evaluates the performance of a classification model.
 
@@ -9,8 +9,8 @@ def evaluate_model(y_true, y_pred):
     :return: None
     """
     print("Accuracy:", accuracy_score(y_true, y_pred))
-    print("Precision:", precision_score(y_true, y_pred, average='binary'))
-    print("Recall:", recall_score(y_true, y_pred, average='binary'))
-    print("F1-Score:", f1_score(y_true, y_pred, average='binary'))
+    print("Precision:", precision_score(y_true, y_pred, average=average))
+    print("Recall:", recall_score(y_true, y_pred, average=average))
+    print("F1-Score:", f1_score(y_true, y_pred, average=average))
     print("\nKlassifikationsbericht:\n")
     print(classification_report(y_true, y_pred))
